8c208be8d30ea0ae8c0b544e7c372f3805c025ba
abidmaqbool20 <abidmaqbool20@gmail.com>
2018-03-14 19:01:31 +0500

    all about pets
    all about pets

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en"> 
+ <head>
+	<title>Pets Professionals</title>
+ 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, ">
+ 
+	<link rel="shortcut icon" href="images/fav.ico" type="image/x-icon">
+ 
+	<link href="https://fonts.googleapis.com/css?family=Poppins%7CQuicksand:500,700" rel="stylesheet">
+ 
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+ 
+	<link href="css/materialize.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+	<link href="css/my-style.css" rel="stylesheet" type="text/css" />
+ 	
+	<link href="css/responsive.css" rel="stylesheet"> 
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+ 	<script src="js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+</head> 
+<body> 
+ 
---
+++ register.php
@@ -0,0 +1,88 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/nav-2.php"); ?>
+	<section class="tz-register">
+			<div class="log-in-pop">
+				<div class="log-in-pop-left">
+					<h1>Hello... <span>{{ name1 }}</span></h1>
+					<p>Don't have an account? Create your account. It's take less then a minutes</p>
+					<h4>Login with social media</h4>
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i> Facebook</a>
+						</li>
+						<li><a href="#"><i class="fa fa-google"></i> Google+</a>
+						</li>
+						<li><a href="#"><i class="fa fa-twitter"></i> Twitter</a>
+						</li>
+					</ul>
+				</div>
+				<div class="log-in-pop-right">
+					<a href="#" class="pop-close" data-dismiss="modal"><img src="images/cancel.html" alt="" />
+					</a>
+					<h4>Create an Account</h4>
+					<p>Don't have an account? Create your account. It's take less then a minutes</p>
+					<form class="s12">
+						<div>
+							<div class="input-field s12">
+								<input type="text" data-ng-model="name1" class="validate">
+								<label>User name</label>
+							</div>
+						</div>
+						<div>
+							<div class="input-field s12">
+								<input type="email" class="validate">
+								<label>Email id</label>
+							</div>
+						</div>
+						<div>
+							<div class="input-field s12">
+								<input type="password" class="validate">
+								<label>Password</label>
+							</div>
+						</div>
+						<div>
+							<div class="input-field s12">
+								<input type="password" class="validate">
+								<label>Confirm password</label>
+							</div>
+						</div>
+						<div>
+							<div class="input-field s4">
+								<input type="submit" value="Register" class="waves-effect waves-light log-in-btn"> </div>
+						</div>
+						<div>
+							<div class="input-field s12"> <a href="login.html">Are you a already member ? Login</a> </div>
+						</div>
+					</form>
+				</div>
+			</div>
+	</section>
+	<!--MOBILE APP-->
+	<section class="web-app com-padd">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 web-app-img"> <img src="images/mobile.png" alt="" /> </div>
+				<div class="col-md-6 web-app-con">
+					<h2>Looking for the Best Service Provider? <span>Get the App!</span></h2>
+					<ul>
+						<li><i class="fa fa-check" aria-hidden="true"></i> Find nearby listings</li>
+						<li><i class="fa fa-check" aria-hidden="true"></i> Easy service enquiry</li>
+						<li><i class="fa fa-check" aria-hidden="true"></i> Listing reviews and ratings</li>
+						<li><i class="fa fa-check" aria-hidden="true"></i> Manage your listing, enquiry and reviews</li>
+					</ul> <span>We'll send you a link, open it on your phone to download the app</span>
+					<form>
+						<ul>
+							<li>
+								<input type="text" placeholder="+01" /> </li>
+							<li>
+								<input type="number" placeholder="Enter mobile number" /> </li>
+							<li>
+								<input type="submit" value="Get App Link" /> </li>
+						</ul>
+					</form>
+					<a href="#"><img src="images/android.png" alt="" /> </a>
+					<a href="#"><img src="images/apple.png" alt="" /> </a>
+				</div>
+			</div>
+		</div>
+	</section>
+<?= include("includes/footer.php"); ?>
