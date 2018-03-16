03b5b7038899d8041457a9cc24599e8b735c73e7
abidmaqbool20 <abidmaqbool20@gmail.com>
2018-03-16 16:12:25 +0500

    changes
    changes

--- trainers.php
+++ trainers.php
@@ -2,129 +2,195 @@
 <?php include("includes/nav-2.php"); ?>
 
 <section class="dir-pa-sp-top">
-		<div class="container com-padd dir-hom-pre-tit">
-			<div class="com-title">
-				<h2>New Businesses in<span> this month</span></h2>
-				<p>Explore some of the best tips from around the world from our partners and friends.</p>
-			</div>
-			<div class="row span-none">
-				<div class="row">
-					<div class="col-md-6" style="padding-right: 0; padding-left: 0;">			 
-						<div class="home-list-pop list-spac list-spac-1">
-							<!--LISTINGS IMAGE-->
-							<div class="col-md-3"> <img src="images/services/s10.jpg" alt=""> </div>
-							<!--LISTINGS: CONTENT-->
-							<div class="col-md-9 home-list-pop-desc inn-list-pop-desc"> <a href="listing-details.html"><h3>Property Luxury Homes</h3></a>
-								<h4>Express Avenue Mall, Los Angeles</h4>
-								<p><b>Address:</b> 28800 Orchard Lake Road, Suite 180 Farmington Hills, U.S.A.</p>
-								<div class="list-number">
+		<div class="container dir-hom-pre-tit">
+			<div class="row com-padd com-padd-redu-bot1">
+				<div class="com-title">
+					<h2>Top trendings in <span>this month</span></h2>
+					<p>Explore some of the best tips from around the world from our partners and friends.</p>
+				</div>
+				<div class="col-md-6">
+					<div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s1.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Effi Furniture Dealers</h3></a>
+								<div class="pg-revi-re"><img src="images/users/8.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
 									<ul>
-										<li><img src="images/icon/phone.png" alt=""> +01 1245 2541, +62 6541 6528</li>
-										<li><img src="images/icon/mail.png" alt=""> localdir@webdir.com</li>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
 									</ul>
-								</div> <span class="home-list-pop-rat">4.2</span>
-								<div class="list-enqu-btn">
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s2.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>NIID Job Training</h3></a>
+								<div class="pg-revi-re"><img src="images/users/2.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
 									<ul>
-										<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i> Write Review</a> </li>
-										<li><a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i> Send SMS</a> </li>
-										<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> Call Now</a> </li>
-										<li><a href="#" data-dismiss="modal" data-toggle="modal" data-target="#list-quo"><i class="fa fa-usd" aria-hidden="true"></i> Get Quotes</a> </li>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s3.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Computer Repair &amp; Services</h3></a>
+								<div class="pg-revi-re"><img src="images/users/3.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
+									<ul>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s4.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Packers and Movers</h3></a>
+								<div class="pg-revi-re"><img src="images/users/4.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
+									<ul>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
 									</ul>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-6" style="padding-right: 0; padding-left: 0;">			 
-						<div class="home-list-pop list-spac list-spac-1">
-							<!--LISTINGS IMAGE-->
-							<div class="col-md-3"> <img src="images/services/s10.jpg" alt=""> </div>
-							<!--LISTINGS: CONTENT-->
-							<div class="col-md-9 home-list-pop-desc inn-list-pop-desc"> <a href="listing-details.html"><h3>Property Luxury Homes</h3></a>
-								<h4>Express Avenue Mall, Los Angeles</h4>
-								<p><b>Address:</b> 28800 Orchard Lake Road, Suite 180 Farmington Hills, U.S.A.</p>
-								<div class="list-number">
+				</div>
+				<div class="col-md-6">
+					<div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s5.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Tour and Travels</h3></a>
+								<div class="pg-revi-re"><img src="images/users/5.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
 									<ul>
-										<li><img src="images/icon/phone.png" alt=""> +01 1245 2541, +62 6541 6528</li>
-										<li><img src="images/icon/mail.png" alt=""> localdir@webdir.com</li>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
 									</ul>
-								</div> <span class="home-list-pop-rat">4.2</span>
-								<div class="list-enqu-btn">
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s6.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Andru Modular Kitchen</h3></a>
+								<div class="pg-revi-re"><img src="images/users/6.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
 									<ul>
-										<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i> Write Review</a> </li>
-										<li><a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i> Send SMS</a> </li>
-										<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> Call Now</a> </li>
-										<li><a href="#" data-dismiss="modal" data-toggle="modal" data-target="#list-quo"><i class="fa fa-usd" aria-hidden="true"></i> Get Quotes</a> </li>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s7.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Rute Skin Care &amp; Treatment</h3></a>
+								<div class="pg-revi-re"><img src="images/users/7.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
+									<ul>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!--POPULAR LISTINGS-->
+						<div class="home-list-pop mar-bot-red-0">
+							<!--POPULAR LISTINGS IMAGE-->
+							<div class="col-md-3"> <img src="images/services/s8.jpg" alt=""> </div>
+							<!--POPULAR LISTINGS: CONTENT-->
+							<div class="col-md-9 home-list-pop-desc"> <a href="#"><h3>Health and Fitness</h3></a>
+								<div class="pg-revi-re"><img src="images/users/1.png" alt="">
+									<p>Melanie <span>252 Reviews , 909 Followers</span>
+										</p><p>
+											</p><div class="list-rat-ch list-room-rati pg-re-rat"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+								</div>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p> <span class="home-list-pop-rat">4.2</span>
+								<div class="hom-list-share">
+									<ul>
+										<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> 52</a> </li>
+										<li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i> 32</a> </li>
+										<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> 420</a> </li>
+										<li><a href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> 570</a> </li>
 									</ul>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-6" style="padding-right: 0; padding-left: 0;">			 
-						<div class="home-list-pop list-spac list-spac-1">
-							<!--LISTINGS IMAGE-->
-							<div class="col-md-3"> <img src="images/services/s10.jpg" alt=""> </div>
-							<!--LISTINGS: CONTENT-->
-							<div class="col-md-9 home-list-pop-desc inn-list-pop-desc"> <a href="listing-details.html"><h3>Property Luxury Homes</h3></a>
-								<h4>Express Avenue Mall, Los Angeles</h4>
-								<p><b>Address:</b> 28800 Orchard Lake Road, Suite 180 Farmington Hills, U.S.A.</p>
-								<div class="list-number">
-									<ul>
-										<li><img src="images/icon/phone.png" alt=""> +01 1245 2541, +62 6541 6528</li>
-										<li><img src="images/icon/mail.png" alt=""> localdir@webdir.com</li>
-									</ul>
-								</div> <span class="home-list-pop-rat">4.2</span>
-								<div class="list-enqu-btn">
-									<ul>
-										<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i> Write Review</a> </li>
-										<li><a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i> Send SMS</a> </li>
-										<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> Call Now</a> </li>
-										<li><a href="#" data-dismiss="modal" data-toggle="modal" data-target="#list-quo"><i class="fa fa-usd" aria-hidden="true"></i> Get Quotes</a> </li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6" style="padding-right: 0; padding-left: 0;">			 
-						<div class="home-list-pop list-spac list-spac-1">
-							<!--LISTINGS IMAGE-->
-							<div class="col-md-3"> <img src="images/services/s10.jpg" alt=""> </div>
-							<!--LISTINGS: CONTENT-->
-							<div class="col-md-9 home-list-pop-desc inn-list-pop-desc"> <a href="listing-details.html"><h3>Property Luxury Homes</h3></a>
-								<h4>Express Avenue Mall, Los Angeles</h4>
-								<p><b>Address:</b> 28800 Orchard Lake Road, Suite 180 Farmington Hills, U.S.A.</p>
-								<div class="list-number">
-									<ul>
-										<li><img src="images/icon/phone.png" alt=""> +01 1245 2541, +62 6541 6528</li>
-										<li><img src="images/icon/mail.png" alt=""> localdir@webdir.com</li>
-									</ul>
-								</div> <span class="home-list-pop-rat">4.2</span>
-								<div class="list-enqu-btn">
-									<ul>
-										<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i> Write Review</a> </li>
-										<li><a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i> Send SMS</a> </li>
-										<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> Call Now</a> </li>
-										<li><a href="#" data-dismiss="modal" data-toggle="modal" data-target="#list-quo"><i class="fa fa-usd" aria-hidden="true"></i> Get Quotes</a> </li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-								 
-								 
-			    </div>    
-				<div class="row">
-					<ul class="pagination list-pagenat">
-						<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a> </li>
-						<li class="active"><a href="#!">1</a> </li>
-						<li class="waves-effect"><a href="#!">2</a> </li>
-						<li class="waves-effect"><a href="#!">3</a> </li>
-						<li class="waves-effect"><a href="#!">4</a> </li>
-						<li class="waves-effect"><a href="#!">5</a> </li>
-						<li class="waves-effect"><a href="#!">6</a> </li>
-						<li class="waves-effect"><a href="#!">7</a> </li>
-						<li class="waves-effect"><a href="#!">8</a> </li>
-						<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a> </li>
-					</ul>
 				</div>
 			</div>
 		</div>
